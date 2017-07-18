3042088227abfbb6b02e00ef2b58dbe10a2fe32a
Aeone Singson <aeone.singson@gmail.com>
2017-07-18 16:52:50 -0700

    added DONE to TODO and estimation

--- starter-code/server.js
+++ starter-code/server.js
@@ -3,10 +3,11 @@
 // DONE: Initialize your project using NPM to create and populate a package.json file
 // Estimated - 5 min | Actual - 15 min
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
+// Estimated - 5 min | Actual - 5 min
 const express = require('express');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
